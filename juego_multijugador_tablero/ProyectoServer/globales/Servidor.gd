4c573e91401b66f3c0eaa08c6ba8312bb74baa03
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2022-02-26 01:23:36 +0100

    Add files via upload

--- juego_multijugador_tablero/ProyectoServer/globales/Servidor.gd
+++ juego_multijugador_tablero/ProyectoServer/globales/Servidor.gd
@@ -1,10 +1,12 @@
 extends Node
-
+# conexiones
 const PORT = 1404
 #const PROTO_NAME = "ludus"
 var peer = WebSocketServer.new()
 onready var label = get_tree().get_root().get_node("Principal/Label")
 
+# partida
+var jugadores_preparados = []
 
 var lista_de_peers 
 func _ready():
@@ -25,6 +27,14 @@ func _peer_disconnected(id):
 	print("Player ", id, "desconectado")
 	lista_de_peers = get_tree().multiplayer.get_network_connected_peers()
 	rpc("rellenar_y_vaciar_lista_espera", lista_de_peers)
+	
+remote func sumar_jugador_activado(jugador_activado):
+	jugadores_preparados.append(jugador_activado)
+	if jugadores_preparados.size() == lista_de_peers.size():
+		print("empezar_partida")
+#		if :
+			
+
 	
 
 #var num = 0
