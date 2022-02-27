29f4b49e44efb7f20d223ec6776c567aad44e3db
brecabre <bravomantonio@gmail.com>
2022-02-27 19:03:56 +0100

    Update Servidor.gd

--- juego_multijugador_tablero/ProyectoCliente/globales/Servidor.gd
+++ juego_multijugador_tablero/ProyectoCliente/globales/Servidor.gd
@@ -3,8 +3,8 @@ onready var menus = get_tree().get_root().get_node("Principal/Menus")
 onready var escena_principal = get_tree().get_root().get_node("Principal")
 var peer = WebSocketClient.new()
 var PROTO_NAME = "bre"
-var url_host = "84.123.115.165"
-#var url_host = "127.0.0.1"
+#var url_host = "84.123.115.165"
+var url_host = "127.0.0.1"
 
 var jugadores_ips = []
 var jugador
@@ -12,7 +12,7 @@ var jugador_preparado = false
 var jugadores_preparados = []
 
 func _ready():
-	peer.connect_to_url("ws://" + url_host +":6968", PoolStringArray([PROTO_NAME]), true)
+	peer.connect_to_url("ws://" + url_host +":1404", PoolStringArray([PROTO_NAME]), true)
 	get_tree().connect("connection_failed", self, "_close_network")
 	get_tree().connect("connected_to_server", self, "_connected")
 	get_tree().set_network_peer(peer)
