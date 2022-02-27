f88fb3a64de6ee20bd03b6e3958e15683edbcd07
brecabre <bravomantonio@gmail.com>
2022-02-27 18:59:08 +0100

    Add files via upload

--- juego_multijugador_tablero/ProyectoCliente/globales/Servidor.gd
+++ juego_multijugador_tablero/ProyectoCliente/globales/Servidor.gd
@@ -1,9 +1,10 @@
 extends Node
 onready var menus = get_tree().get_root().get_node("Principal/Menus")
+onready var escena_principal = get_tree().get_root().get_node("Principal")
 var peer = WebSocketClient.new()
-var PROTO_NAME = "ludus"
-#var url_host = "84.123.115.165"
-var url_host = "127.0.0.1"
+var PROTO_NAME = "bre"
+var url_host = "84.123.115.165"
+#var url_host = "127.0.0.1"
 
 var jugadores_ips = []
 var jugador
@@ -11,7 +12,7 @@ var jugador_preparado = false
 var jugadores_preparados = []
 
 func _ready():
-	peer.connect_to_url("ws://" + url_host +":1404", PoolStringArray([PROTO_NAME]), true)
+	peer.connect_to_url("ws://" + url_host +":6968", PoolStringArray([PROTO_NAME]), true)
 	get_tree().connect("connection_failed", self, "_close_network")
 	get_tree().connect("connected_to_server", self, "_connected")
 	get_tree().set_network_peer(peer)
@@ -23,16 +24,6 @@ func _close_network():
 func _connected():
 	print("connection server")
 
-#remote func rellenar_y_vaciar_lista_espera(_jugadores_ips):	
-#	jugadores_ips = _jugadores_ips
-#	var x = 0
-#	for i in 4:
-#		if jugadores_ips.size() <= x:
-#			menus.cambiar_labels_players(x,"vacio")
-#			x +=1
-#		else:
-#			menus.cambiar_labels_players(x,str(jugadores_ips[x]))
-#			x +=1
 
 func preparado():
 	jugador_preparado = true 
@@ -41,7 +32,13 @@ func preparado():
 	print("Activa jugador" , get_tree().get_network_unique_id())
 	
 remote func rellenar_y_vaciar_lista_espera(_jugadores_ips):
-	menus.rellenar_y_vaciar_HBoxPlayers(_jugadores_ips)
+	jugadores_ips = _jugadores_ips
+	menus.rellenar_y_vaciar_HBoxPlayers(jugadores_ips)
 	
-remote func listos():
-	print("start partida")
+
+
+remote func establecer_jugador(n):
+	print("star partida")
+	jugador = n
+	print("jugador: ", n)
+	escena_principal.preparar_partida()
